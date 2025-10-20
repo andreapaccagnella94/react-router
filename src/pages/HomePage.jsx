075cc9ea828375c812dfaa4ad4112faab292e136
andreapaccagnella94 <andreapacca94@live.it>
2025-10-20 19:54:03 +0200

    add organized the overall product page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,3 @@
-/*
-Consegna
-
-Creiamo il frontend del nostro mini e-commerce e le sue pagine principali!
-Useremo Fake Store API come backend fittizio per simulare i dati dei prodotti.
-
-https://fakestoreapi.com/
-
-Obiettivi
-
-Installiamo React Router DOM: npm i react-router-dom
-Creiamo almeno 3 pagine principali:
-Homepage (con un messaggio di benvenuto o immagine promozionale)
-Chi siamo
-Prodotti (pagina che mostrerà la lista dei prodotti prendendoli da https://fakestoreapi.com/products)
-Implementiamo una Navbar visibile in tutte le pagine per navigare tra di esse
-
-Bonus
-
-Centralizziamo la Navbar usando un componente Layout
-Gestiamo la classe active per i link attivi nella Navbar
-*/
 
 
 export default function HomePage() {
@@ -27,14 +5,14 @@ export default function HomePage() {
     return (
         <>
 
-            <main>
-                <div className="p-5 mb-4 bg-light rounded-3">
+            <main className="min-vh-100">
+                <div className="p-5 my-4 bg-light rounded-3">
                     <div className="container-fluid py-5">
                         <h1 className="display-5 fw-bold"> </h1>
                         <p className="col-md-8 fs-4">
-                            Using a series of utilities, you can create this jumbotron, just
-                            like the one in previous versions of Bootstrap. Check out the
-                            examples below for how you can remix and restyle it to your liking.
+                            {/* messagio di benventuo del sito finto di un e-commerce */}
+                            This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+                            For a e-commerce site, you might use this area to showcase a new product, highlight a special offer, or provide important updates to your customers.
                         </p>
                         <button className="btn btn-primary btn-lg" type="button">
                             Example button
--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,55 @@
-import AppNav from "../components/AppNav"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 export default function ProductsPage() {
+
+    const initialPage = 'https://fakestoreapi.com/products'
+    // make a state variable where store the products list
+    const [products, setProducts] = useState([])
+    const [currentPage, setCurrentPage] = useState(initialPage)
+
+    function fetchData(page) {
+        axios
+            .get("https://fakestoreapi.com/products")
+            .then(res => {
+                // log per vedere
+                // console.log(res.data);
+                const products = res.data
+                setProducts(products)
+            })
+            .catch(err => {
+                console.error(err.message)
+            })
+    }
+
+    useEffect(() => {
+        fetchData(currentPage)
+    }, [])
 
     return (
         <>
             <main>
+                <div className="container text-center my-5">
+                    <h1>PRODUCTS</h1>
+                </div>
+                <section>
+                    <div className="container">
+                        <div className="row g-4">
+                            {products.map(product =>
+                                <div className="col-12 col-sm-6 col-md-4 col-lg-3" key={product.id}>
+                                    <div className="card h-100">
+                                        <img src={product.image} className="card-img-top"></img>
+                                        <div className="card-body">
+                                            <h5 className="card-title">{product.title}</h5>
+                                            <p className="card-text">{product.description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            )}
 
-                <h1>products</h1>
+                        </div>
+                    </div>
+                </section>
             </main>
         </>
     )
